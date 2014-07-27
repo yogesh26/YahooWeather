e14e7b55aea6d00711281ad9b529d4685263aad1
yogesh <yogesh_26@hotmail.com>
2014-07-27 20:59:34 +0200

    Local branch created

---
+++ YahooWeather/MyMapClass.h
@@ -0,0 +1,13 @@
+//
+//  MyMapClass.h
+//  YahooWeather
+//
+//  Created by Yogesh Kumar on 2014/07/27.
+//  Copyright (c) 2014 Yogesh Kumar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyMapClass : NSObject
+
+@end
---
+++ YahooWeather/MyMapClass.m
@@ -0,0 +1,13 @@
+//
+//  MyMapClass.m
+//  YahooWeather
+//
+//  Created by Yogesh Kumar on 2014/07/27.
+//  Copyright (c) 2014 Yogesh Kumar. All rights reserved.
+//
+
+#import "MyMapClass.h"
+
+@implementation MyMapClass
+
+@end
